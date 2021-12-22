1a83f6d157b622c8ce4342870c4eea9287154bbd
naritayusuke <yuusukenarita8@gmail.com>
2021-12-22 17:46:48 +0900

    [add]create

--- blog/app/Http/Controllers/PostController.php
+++ blog/app/Http/Controllers/PostController.php
@@ -12,4 +12,18 @@ class PostController extends Controller
     {
         return view('posts/index')->with(['posts'=>$post->getPaginateByLimit()]);
     }
+    /**
+ * 特定IDのpostを表示する
+ *
+ * @params Object Post // 引数の$postはid=1のPostインスタンス
+ * @return Reposnse post view
+ */
+    public function show(Post $post)
+    {
+        return view('posts/show')->with(['post' => $post]);
+    }
+    public function create(){
+        return view('posts/create');
+    }
+    
 }
