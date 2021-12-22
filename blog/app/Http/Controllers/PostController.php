c8c8e8aa2b2a3ca36719723e88f4779912976a26
naritayusuke <yuusukenarita8@gmail.com>
2021-12-22 22:10:05 +0900

    [fin]kl8.4

--- blog/app/Http/Controllers/PostController.php
+++ blog/app/Http/Controllers/PostController.php
@@ -3,33 +3,29 @@
 namespace App\Http\Controllers;
 
 use App\Post;
-
-use Illuminate\Http\Request;
+use App\Http\Requests\PostRequest; // useする
 
 class PostController extends Controller
 {
     public function index(Post $post)
     {
-        return view('posts/index')->with(['posts'=>$post->getPaginateByLimit()]);
+        return view('posts/index')->with(['posts' => $post->getPaginate()]);
     }
-    /**
- * 特定IDのpostを表示する
- *
- * @params Object Post // 引数の$postはid=1のPostインスタンス
- * @return Reposnse post view
- */
+
     public function show(Post $post)
     {
         return view('posts/show')->with(['post' => $post]);
     }
-    public function create(){
+
+    public function create()
+    {
         return view('posts/create');
     }
-    public function store(Request $request,Post $post){
-        
-        $input=$request['post'];
+
+    public function store(Post $post, PostRequest $request) // 引数をRequest->PostRequestにする
+    {
+        $input = $request['post'];
         $post->fill($input)->save();
         return redirect('/posts/' . $post->id);
     }
-    
 }
--- blog/app/Post.php
+++ blog/app/Post.php
@@ -12,7 +12,7 @@ class Post extends Model
         'body',
     ];
     
-    public function getPaginateByLimit(int $limit_count = 5)
+    public function getPaginate(int $limit_count = 5)
     {
         // updated_atで降順に並べたあと、limitで件数制限をかける
         return $this->orderBy('updated_at', 'DESC')->paginate($limit_count);
